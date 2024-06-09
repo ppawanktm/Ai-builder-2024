5a4d9fe70b2946b3e99a2953c6c917cedaaa012e
pawan jakkaew <163524399+ppawanktm@users.noreply.github.com>
2024-06-09 13:55:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def get_model(model_name:str):
 
 # Streamlit app
 def main():
-    st.title("Image Classification with Hugging Face")
+    st.title("Image Classification for Eye-Diseases")
     st.write("Upload an image for classification.")
 
     # Image upload
