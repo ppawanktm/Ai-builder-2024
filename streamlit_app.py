f018e2d0b952eae047435d8b6e7d31678312e271
pawan jakkaew <163524399+ppawanktm@users.noreply.github.com>
2024-06-10 22:47:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,31 +8,10 @@ import pandas as pd
 def get_model(model_name: str):
     return pipeline("image-classification", model=model_name)
 
-# Custom CSS to increase the size of the DataFrame container
-def add_custom_css():
-    st.markdown(
-        """
-        <style>
-        .dataframe-container {
-            width: 100%;
-            height: 600px;
-            overflow: auto;
-        }
-        .dataframe-container .stDataFrame {
-            font-size: 20px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
 # Streamlit app
 def main():
     st.title("Image Classification for Eye Diseases")
     st.write("Upload an image for classification.")
-
-    # Add custom CSS
-    add_custom_css()
 
     # Image upload
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
@@ -51,12 +30,9 @@ def main():
                 'Score': scores
             })
 
-            # Display the DataFrame in a container with custom CSS
-            st.markdown('<div class="dataframe-container">', unsafe_allow_html=True)
-            st.dataframe(df.style.set_properties(**{
-                'text-align': 'left'
-            }))
-            st.markdown('</div>', unsafe_allow_html=True)
+            # Display the DataFrame
+            st.write("Predicted Classes:")
+            st.dataframe(df)
 
 if __name__ == "__main__":
     model_name = "ttangmo24/vit-base-classification-Eye-Diseases"
